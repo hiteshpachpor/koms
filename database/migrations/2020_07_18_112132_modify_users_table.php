474fcfa2ddc62f828aa4714cfba875d819a97752
Hitesh Pachpor <hiteshspac@gmail.com>
2020-07-19 15:11:38 +0530

    fix(db-migrations): made phone number a nullable field

--- database/migrations/2020_07_18_112132_modify_users_table.php
+++ database/migrations/2020_07_18_112132_modify_users_table.php
@@ -14,7 +14,7 @@ class ModifyUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone', 16);
+            $table->string('phone', 16)->nullable();
         });
     }
 
